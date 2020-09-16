0e2df3d40b57aed9c27a277f14c23370ad3a2f11
Thiago <thiagopereira2109@gmail.com>
2020-09-15 23:29:20 -0300

    Criando a Activity para exibir o logo no inicio

---
+++ AplicativoGPS/app/src/main/java/com/example/aplicativogps/SmashScreen.java
@@ -0,0 +1,35 @@
+package com.example.aplicativogps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SmashScreen extends Activity {
+
+    // Timer da splash screen
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity principal
+                Intent i = new Intent(SmashScreen.this,
+                        MainActivity.class);
+                startActivity(i);
+
+                // Fecha esta activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
