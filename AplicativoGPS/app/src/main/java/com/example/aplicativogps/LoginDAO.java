c86de9a0307972b9e16db9a51ed12c5a299a8a08
Thiago <thiagopereira2109@gmail.com>
2020-10-01 22:55:24 -0300

    alteracao na dao para pegar sequencial

--- AplicativoGPS/app/src/main/java/com/example/aplicativogps/LoginDAO.java
+++ AplicativoGPS/app/src/main/java/com/example/aplicativogps/LoginDAO.java
@@ -9,46 +9,77 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Date;
+import java.util.Map;
+
 public class LoginDAO {
     private DatabaseReference mDataBase;
-    private boolean controleUser;
+    private boolean retorno;
+    private int retornoId;
 
     public LoginDAO() {
         this.mDataBase = FirebaseDatabase.getInstance().getReference();
     }
 
-    public boolean findUsers(final String nomeUsuario) {
-        mDataBase = FirebaseDatabase.getInstance().getReference().child("login");
-        Query lastQuery = mDataBase.orderByKey();
-        lastQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+    public boolean findUsers(final String usuario) {
+
+        mDataBase.child("login").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot data : dataSnapshot.getChildren())
-                {
-                    String result = (String) data.getValue();
-                    if (result.equals(nomeUsuario)){
-                        controleUser = true;
-                    } else {
-                        controleUser = false;
-                    }
-
-                }
+                if (dataSnapshot.getValue() != null)
+                    retorno = getAllUsers((Map<String, String>) dataSnapshot.getValue(), usuario);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
-            }});
-        return  controleUser;
+            }
+        });
+        return retorno;
     }
 
     public boolean insereUsuario(String nomeUsuario) {
         try {
-            mDataBase.child("login").child("nome").setValue(nomeUsuario);
+            Integer resposta = getLastId();
+            mDataBase.child("login").child(String.valueOf(1)).child("nome").setValue(nomeUsuario);
             return true;
         } catch (Exception e) {
-            return  false;
+            return false;
         }
+    }
+
+    private boolean getAllUsers(Map<String, String> users, String usuarioParam) {
+        for (Map.Entry<String, String> entry : users.entrySet()) {
+            String usuario = entry.getValue();
+            if (usuario == usuarioParam) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int getLastId(){
+        mDataBase.child("login").orderByKey().limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null){
+                    getId((Map<String, String>) dataSnapshot.getValue());
+                    retornoId = 0;
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return  retornoId;
+    }
+    private boolean getId(Map<String, String> users) {
+        for (Map.Entry<String, String> entry : users.entrySet()) {
+            String usuario = entry.getValue();
+        }
+        return false;
     }
 
 }
